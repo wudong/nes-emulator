e3d284d6c45f3a4f71a7607bd364e4abe3fd7536
Wudong Liu <wudong.liu@gmail.com>
2022-03-14 17:36:22 +0000

    init commit

---
+++ src/cpu.rs
@@ -0,0 +1,68 @@
+pub struct CPU {
+    pub register_a: u8;,
+    pub status: u8,
+    pub program_counter: u16,
+}
+
+impl CPU {
+    pub fn new()-> Self {
+        CPU {
+            register_a: 0,
+            status: 0,
+            program_counter: 0,
+        }
+    }
+
+    pub fn interpret(&mut self, program: Vec<u8>) {
+        self.program_counter = 0;
+        loop {
+            let opcode = program[self.program_counter as usize];
+            self.program_counter +=1;
+
+            match opcode {
+                0xA9 => { // LDA
+                    let param = program[self.program_counter as usize];
+                    self.program_counter +=1;
+                    self.register_a = param;
+                    
+                    if self.register_a == 0 {
+                        self.status = self.status | 0b0000_0010;                      
+                    }else {
+                        self.status = self.status & 0b1111_1101;
+                    }
+
+                    if self.register_a & 0b1000_0000 !=0 {
+                        self.status = self.status | 0b1000_0000;
+                    }else {
+                        self.status = self.status & 0b0111_1111;
+                    }
+                },
+                0x00 => { //BRK
+                    return;
+                }
+                _=> todo!()
+            }
+        }
+    }
+}
+
+#[cfg(test)]
+mod test {
+   use super::*;
+ 
+   #[test]
+   fn test_0xa9_lda_immidiate_load_data() {
+       let mut cpu = CPU::new();
+       cpu.interpret(vec![0xa9, 0x05, 0x00]);
+       assert_eq!(cpu.register_a, 0x05);
+       assert!(cpu.status & 0b0000_0010 == 0b00);
+       assert!(cpu.status & 0b1000_0000 == 0);
+   }
+
+    #[test]
+    fn test_0xa9_lda_zero_flag() {
+        let mut cpu = CPU::new();
+        cpu.interpret(vec![0xa9, 0x00, 0x00]);
+        assert!(cpu.status & 0b0000_0010 == 0b10);
+    }
+}
